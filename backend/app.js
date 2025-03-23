b65a76093d947940071e4043f899dcc51105d38f
iamsriramm <151650263+iamsriramm@users.noreply.github.com>
2025-03-23 16:29:46 +0530

    check

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,9 @@ const port=process.env.PORT || 5000
 dotEnv.config()
 app.use(cors())
 app.use(express.json())
-
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel!");
+});
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
